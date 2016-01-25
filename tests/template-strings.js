f532352e222df6730ce5dfbf409b7c1ba856b189
Robert Ayers <rsheldon3ayers@gmail.com>
2016-01-25 15:28:23 -0600

    finished template strings

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+
 describe('template strings', function() {
   it('should be a string', function () {
     var string = 'abc';
@@ -6,4 +7,30 @@ describe('template strings', function() {
     expect(string).to.be.a('string');
 
   });
-});
+  it('can evaluate variables', function () {
+    var bar = `baz`;
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+
+    });
+    it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal('The date is: ' + date);
+    });
+    it('can evaluate expressions', function () {
+     var string = 2;
+
+     expect(string).to.equal(1 + 1);
+
+
+    });
+    it('can test multi-line strings', function () {
+    var string = `Hello\n
+                  World`;
+
+    expect(string).to.equal('Hello\n                World');
+
+    });
+  });
